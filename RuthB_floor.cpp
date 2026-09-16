57750b0e3ae070e429a3e04a22c76ff192fb9171
Ruth-B55 <ruthkemigisha290@gmail.com>
2026-09-16 14:33:32 +0300

    Add files via upload

---
+++ RuthB_floor.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+//function to calcutale floor of a number
+int myFloor(double x)
+{
+    int integerPart= (int)x;
+
+    //for negative nubers with a decimal part,
+    //move to the next smaller integer.
+    if(x < integerPart){
+        integerPart--;
+    }
+    return integerPart;
+}
+int main(){
+    double x;
+    cout << "Enter a number: ";
+    cin >> x;
+
+    int result = myFloor(x);
+
+    cout << "Floor of " << x << " =" << result << endl;
+
+    return 0;
+}
